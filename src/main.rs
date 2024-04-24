112022e58d2fd94fc87e1bffc6e6ef55e603b3e9
twallie <thomaswallsmith@gmail.com>
2024-04-23 20:38:23 -0400

    feat: outputting names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser)]
+struct CLI {
+    #[arg(short = 'l', long = "lexx")]
+    lexx: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let response = match knightpark::get_response().await {
+        Ok(v) => v,
+        Err(_) => {
+            println!("Error contacting Parking API.");
+            return;
+        }
+    };
+
+    response.into_iter().for_each(|element| {
+        println!("{:?}", element.location.name);
+    });
 }
